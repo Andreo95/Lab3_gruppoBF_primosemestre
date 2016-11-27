5928bc208e35096b88b1138dee46e13c487501f9
Andreo95 <andrea.luzio95@gmail.com>
2016-11-27 18:07:09 +0100

    un poco di cosette

--- 07/slewrate.py
+++ 07/slewrate.py
@@ -15,11 +15,27 @@ path=os.path.join(folder, datafile)
 o=Oscillografo.OscilloscopeData(path)
 o.plot()
 
-f=Fitter(o.T2 ,o.CH2, mme(o.T2 , "time", "oscil"), np.ones(len(o.CH2))*o.dCH2)
+f=Fitter(o.T2 ,o.CH2, np.ones(len(o.CH2)), np.ones(len(o.CH2)))
+fun=createline("const")
+fun.mask = (o.T2 < -1.6e-6)# o.T2>5e-7
+f.fit(fun)
+chiq=sum(fun.resd**2)
+mult=(chiq/len(fun.resd))**0.5
+print("mult={0}".format(mult))
+
+
+
+f=Fitter(o.T2 ,o.CH2, np.ones(len(o.CH2))*(np.amax(o.T2)-np.amin(o.T2))/5000 , np.ones(len(o.CH2))*mult)
 fun=createline()
 fun.mask = (o.T2 < 0) & (o.T2 > -1.2e-6)
 f.fit(fun)
+#chiq=sum(fun.resd**2/(np.ones(len(fun.mask))*mult)**2)
 
-Graph.from_fitter(f).draw(fun, resid=True)
+g=Graph.from_fitter(f)
+g.title="Slow ratio e discriminatore"
+g.labelX="tempo [s]"
+g.labelY="tensione [V]"
+g.draw(fun, resid=True)
+
 
 plt.show()
