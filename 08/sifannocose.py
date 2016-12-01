57dddea88076e73813e222ee64499d25b089f6ff
IanMaayrkas <vale.loma@hotmail.it>
2016-12-01 18:52:43 +0100

    progress!

--- 08/sifannocose.py
+++ 08/sifannocose.py
@@ -18,11 +18,9 @@ def newsine():
 	return sinewave
 
 myreg = re.compile("parte1_(?P<freq>[0-9]+).csv")
-
 csvdata = os.path.join(folder, "Dati", "parte1")
 
-results1 = []
-results2 = []
+results = []
 
 for filename in os.listdir(csvdata):
 	w = myreg.match(filename)
@@ -30,20 +28,49 @@ for filename in os.listdir(csvdata):
 	o = Oscillografo.OscilloscopeData(os.path.join(csvdata, filename))
 
 	f = Fitter(o.T2, o.CH2, np.ones(len(o.CH2))*mme(np.amax(o.T2)-np.amin(o.T2), 'time', 'oscil'), np.ones(len(o.CH2))*o.dCH2)
-	fun = newsine()
-	fun.pars = [2*np.pi*freq, 0, 1, 0]
-	f.fit(fun, verbose=False)
-	results1.append(fun.pars)
+	vout = newsine()
+	vout.pars = [2*np.pi*freq, 0, 1, 0]
+	f.fit(vout, verbose=False)
+	if vout.pars[2] < 0:
+		vout.pars[2] *= -1
+		if vout.pars[1] > 0:
+			vout.pars[1] -= np.pi
+		else:
+			vout.pars[1] += np.pi
+
 	f = Fitter(o.T1, o.CH1, np.ones(len(o.CH1))*mme(np.amax(o.T1)-np.amin(o.T1), 'time', 'oscil'), np.ones(len(o.CH2))*o.dCH1)
-	f.fit(fun, verbose=False)
-	results2.append(fun.pars)
+	vin = newsine()
+	vin.pars = [2*np.pi*freq, 0, 1, 0]
+	f.fit(vin, verbose=False)
+	if vin.pars[2] < 0:
+		vin.pars[2] *= -1
+		if vin.pars[1] > 0:
+			vin.pars[1] -= np.pi
+		else:
+			vin.pars[1] += np.pi
 
-results2 = np.array(results2)
-results1 = np.array(results1)
+	phase = vout.pars[1] - vin.pars[1]
+	dphase = np.sqrt(vout.sigmas[1]**2 + vin.sigmas[1]**2)
+	Av = 20 * np.log10(vout.pars[2] / vin.pars[2])
+	dAv = 20 * np.log10(np.e) * np.sqrt(vout.sigmas[2]**2 / vout.pars[2]**2 + vin.sigmas[2]**2 / vin.pars[2]**2)
+	avgf = (vout.pars[0]/vout.sigmas[0]**2 + vin.pars[0]/vin.sigmas[0]**2)/(vout.sigmas[0]**-2 + vin.sigmas[0]**-2)/(2*np.pi)
+	df = 1 / np.sqrt(vout.sigmas[0]**-2 + vin.sigmas[0]**-2) / (2*np.pi)
 
-phase = results1[:,1] - results2[:,1]
-freqs = (results1[:,0] + results2[:,0])/4/np.pi
-gain = 20*np.log10(np.abs(results1[:,2] / results2[:,2]))
+	results.append((avgf, df, phase, dphase, Av, dAv))
+
+results = np.array(results).T
+
+freqs, dfreqs = results[0:2]
+phase, dphase = results[2:4]
+gain, dgain = results[4:6]
+
+sfasamento = Graph(freqs, phase, dfreqs, dphase)
+aperbeta = Graph(freqs, gain, dfreqs, dgain)
+
+sfasamento.draw()
+aperbeta.draw()
+
+plt.show()
 
 # sorter = np.argsort(freqs)
 # freqs, phase, gain = np.array([freqs, phase, gain])[:,sorter]
